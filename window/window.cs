314fdbc24a11e684621f8e5b6a73514f9423d80b
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2024-06-22 18:42:48 -0400

    first commit

---
+++ window/window.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class window : Node
+{
+	public override void _Ready()
+	{}
+	
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("fullscreen"))
+		{
+			if (DisplayServer.WindowGetMode() == DisplayServer.WindowMode.Fullscreen)
+				DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+			else DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+		}
+	}
+}
