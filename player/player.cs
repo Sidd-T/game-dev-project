cb4bb542ee74a71b544889aada0b2b9d406da6f6
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2024-06-23 20:09:55 -0400

    add basic pause menu

--- player/player.cs
+++ player/player.cs
@@ -7,10 +7,12 @@ public partial class player : CharacterBody3D
 	public bool gamepadMode;
 	private Marker3D cameraCenter {get; set;}
 	private MeshInstance3D collisionBody {get; set;}
+	
 	[Export] float speed = 6.0f;
 	[Export] float sprintSpeed = 10.0f;
-	[Export] float jumpVelocity = 5f;
+	[Export] float jumpVelocity = 7f;
 	[Export] float gravity = 14f;
+	
 	[Export(PropertyHint.Range, "0.1,1.0")] float camSensitivity = 0.3f;
 	[Export(PropertyHint.Range, "-90,0,1")] float minCamPitch = -50f;
 	[Export(PropertyHint.Range, "0,90,1")] float maxCamPitch = 30f;
@@ -22,10 +24,9 @@ public partial class player : CharacterBody3D
 
 	public override void _Process(double delta)
 	{
-		//uncapturing the mouse disables PC movement but still simulates gravity
-		if (Input.IsActionJustPressed("uncapture_mouse")) Input.MouseMode = Input.MouseModeEnum.Visible;
-		if (Input.IsMouseButtonPressed(MouseButton.Left)) Input.MouseMode = Input.MouseModeEnum.Captured;
-		
+		//capture cursor
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+
 		/**body rotation is in regular process because it lags in physicsprocess and is more a animation anyway
 			maybe rotate extra collisions separately for invisible lag that may occur**/
 		if (direction != Vector3.Zero && Input.MouseMode == Input.MouseModeEnum.Captured | gamepadMode)
@@ -34,6 +35,7 @@ public partial class player : CharacterBody3D
 			bodyRotation.Y = Mathf.LerpAngle(bodyRotation.Y,Mathf.Atan2(-direction.X, -direction.Z), (float)delta * speed);
 			collisionBody.Rotation = bodyRotation;
 		}
+		
 		//camera gamepad part (needs constant movement so _Input event wouldn't work with this like mouseinput)
 		gpCamVector = Input.GetVector("gp_cam_left", "gp_cam_right", "gp_cam_up", "gp_cam_down");
 		if(gpCamVector != Vector2.Zero)
@@ -45,22 +47,26 @@ public partial class player : CharacterBody3D
 			cameraCenter.RotationDegrees = camRot;
 		}
 	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-
+		bool isSprinting = false;
+	
+		//in air
 		if (!IsOnFloor())
 			velocity.Y -= gravity * (float)delta; //characterbodys don't have physic simulations by default like rigidbody
-	
+		
+		//jumping
 		if (Input.IsActionJustPressed("move_jump") && IsOnFloor() && Input.MouseMode == Input.MouseModeEnum.Captured | gamepadMode)
 			velocity.Y = jumpVelocity;
+		
+		//sprinting
+		if (Input.IsActionPressed("sprint") && IsOnFloor())
+			isSprinting = true;
 
 		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
 		direction = new Vector3(inputDir.X, 0, inputDir.Y).Rotated(Vector3.Up, GetNode<Marker3D>("camera_center").Rotation.Y).Normalized(); //rotates the input direction with camera rotation
-		
-		bool isSprinting = false;
-		if (Input.IsActionPressed("sprint"))
-			isSprinting = true;
 		
 		if (direction != Vector3.Zero && Input.MouseMode == Input.MouseModeEnum.Captured | gamepadMode)
 		{
@@ -75,6 +81,7 @@ public partial class player : CharacterBody3D
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	
 	public override void _Input(InputEvent @event)
 	{
 		gamepadMode = @event is InputEventJoypadButton | @event is InputEventJoypadMotion;
